5644109af269d92c0dbe505154c2dc8531d34d95
GeneralOy <Guest@wts5.attlocal.net>
2015-09-10 16:56:48 -0700

    robot about done
    robot almost done

--- src/Project/FeedTortoise2Player.java
+++ src/Project/FeedTortoise2Player.java
@@ -1,73 +1,92 @@
 package Project;
+
 //Copyright Wintriss Technical Schools 2013 
 import java.awt.Component;
 import java.awt.KeyEventDispatcher;
 import java.awt.KeyboardFocusManager;
 import java.awt.Label;
+import java.awt.Window;
 import java.awt.event.KeyEvent;
 import java.util.Random;
+
+import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
-public class FeedTortoise2Player implements KeyEventDispatcher
-{
-	Robot robot = new Robot();
-	Robot robot2 = new Robot();
+
+public class FeedTortoise2Player implements KeyEventDispatcher {
+	public static int x = 0;
+	public boolean foodFound;
+	public static Robot robot = new Robot();
+	public static Robot robot2 = new Robot();
 	int tortoiseLocationX = robot.getX();
 	int tortoiseLocationY = robot.getY();
-	int x2 = robot2.getY();
+	int x2 = robot2.getX();
 	int y2 = robot2.getY();
-	int foodLocationX;
-	int foodLocationY;
-
+	int randomTPX = new Random().nextInt(900);
+	int randomTPY = new Random().nextInt(500);
 	// 2. Choose a character for your food or leave it as *
 	Component food = new Label("*");
 
-	private void goUp()
-	{
-		robot.moveTo(robot.getX(), robot.getY() - 20);
-	}
-	private void goDown()
-	{
-		robot.moveTo(robot.getX(), robot.getY() + 20);
-	}
-	private void goLeft()
-	{
-		robot.moveTo(robot.getX() - 20, robot.getY());
-	}
-	private void goRight()
-	{
-		robot.moveTo(robot.getX() + 20, robot.getY());
-	}
-	private void goUp2()
-	{
-		robot2.moveTo(robot2.getX(), robot2.getY() - 20);
-	}
-	private void goDown2()
-	{
-		robot2.moveTo(robot2.getX(), robot2.getY() + 20);
-	}
-	private void goLeft2()
-	{
-		robot2.moveTo(robot2.getX() + 20, robot2.getY());
-	}
-	private void goRight2()
-	{
-		robot2.moveTo(robot2.getX() - 20, robot2.getY());
-	}
-	private void checkIfFoodFound() throws Exception
-	{
-		this.tortoiseLocationX = robot.getX();
-		this.tortoiseLocationY = robot.getY();
-		this.x2 = robot2.getY();
-		this.y2 = robot2.getY();
-		
-		// 7. if tortoise is near the food
-			// say something. Hint: Runtime.getRuntime().exec("say yum")
+	private void telePort() {
+		robot2.setX(randomTPX);
+		robot2.setY(randomTPY);
+		randomTPX = new Random().nextInt(1500);
+		randomTPY = new Random().nextInt(900);
 	}
 
-	public boolean dispatchKeyEvent(KeyEvent e)
-	{
-		if (e.getID() == KeyEvent.KEY_PRESSED)
-		{
+	private void goUp() {
+		robot.moveTo(robot.getX(), robot.getY() - 20);
+	}
+
+	private void goDown() {
+		robot.moveTo(robot.getX(), robot.getY() + 20);
+	}
+
+	private void goLeft() {
+		robot.moveTo(robot.getX() - 20, robot.getY());
+	}
+
+	private void goRight() {
+		robot.moveTo(robot.getX() + 20, robot.getY());
+	}
+
+	private void goUp2() {
+		robot2.moveTo(robot2.getX(), robot2.getY() - 20);
+	}
+
+	private void goDown2() {
+		robot2.moveTo(robot2.getX(), robot2.getY() + 20);
+	}
+
+	private void goLeft2() {
+		robot2.moveTo(robot2.getX() + 20, robot2.getY());
+	}
+
+	private void goRight2() {
+		robot2.moveTo(robot2.getX() - 20, robot2.getY());
+	}
+
+	private void checkIfFoodFound() {
+		if(x == 0){
+			x2 = robot2.getX();
+			y2 = robot2.getY();
+			int x1 = robot.getX();
+			int y1 = robot.getY();
+			if ((x2 + 20) > (x1 - 50) && (x2 - 20) < (x1 + 50)) {
+				if ((y2 + 20) > (y1 - 50) && (y2 - 20) < (y1 + 50)) {
+					System.out.println("you caught him!");
+					foodFound = true;
+					JOptionPane.showMessageDialog(null, "Blue Wins!");
+				}
+			} else {
+				foodFound = false;
+			}
+		}
+	}
+
+	public boolean dispatchKeyEvent(KeyEvent e) {
+		if (e.getID() == KeyEvent.KEY_PRESSED) {
+				checkIfFoodFound();
 			if (e.getKeyCode() == KeyEvent.VK_RIGHT)
 				goRight();
 			if (e.getKeyCode() == KeyEvent.VK_LEFT)
@@ -84,81 +103,72 @@ public class FeedTortoise2Player implements KeyEventDispatcher
 				goUp2();
 			if (e.getKeyCode() == KeyEvent.VK_S)
 				goDown2();
+			if (e.getKeyCode() == KeyEvent.VK_SPACE)
+				telePort();
+			/**
+			 * Hint: "e.getKeyCode() == KeyEvent.VK_T" is TRUE when T is pressed
+			 * 8. if the W key is pressed, call the method moveFoodUp();
+			 * 
+			 * 11. Do the same for left, right, and down. You can choose your
+			 * own keys if you like. You will need to make new methods for
+			 * moving the food.
+			 * 
+			 * 12. If the secret key is pressed Call the method
+			 * moveFoodToARandomLocation
+			 * 
+			 * Awesome points(finish everything else first): The person
+			 * controlling the food has a big disadvantage they have to keep
+			 * pressing buttons. While the turtle player and hold the arrow
+			 * keys. Can you think you think of a way to make this game more
+			 * fair for the food? Hint: e.getID() == KeyEvent.KEY_PRESSED is
+			 * true when a key is first pressed and e.getID() ==
+			 * KeyEvent.KEY_RELEASED is true when a key is released.
+			 **/
 
-			/**Hint: "e.getKeyCode() == KeyEvent.VK_T" is TRUE when T is pressed
-			 8. if the W key is pressed,
-				 call the method moveFoodUp();
-			
-			11. Do the same for left, right, and down. You can choose your own keys if you like. You will need to make new methods for moving the food.
-			
-			12. If the secret key is pressed
-				 Call the method moveFoodToARandomLocation
-
-			 Awesome points(finish everything else first): The person controlling the food has a big disadvantage they have to keep pressing buttons. While the turtle player and hold the arrow keys. Can you think you think of a way to make this game more fair for the food? Hint: e.getID() == KeyEvent.KEY_PRESSED is true when a key is first pressed and e.getID() == KeyEvent.KEY_RELEASED is true when a key is released. **/
-
-			
 		}
 		return processEvent();
 	}
-	
 
-	private void moveFoodUp()
-	{
+	private void moveFoodUp() {
 		System.out.println("Move food up!");
-		//9. decrement foodLocationY 
+		// 9. decrement foodLocationY
 
-		//10. call the moveFood() method
+		// 10. call the moveFood() method
 
 	}
-	
-	private void moveFoodToARandomLocation()
-	{
-		//foodLocationX = new Random().nextInt(window.getSize().width);
-		//foodLocationY = new Random().nextInt(window.getSize().height);
-		moveFood();
+
+	private void moveFoodToARandomLocation() {
+		// foodLocationX = new Random().nextInt(window.getSize().width);
+		// foodLocationY = new Random().nextInt(window.getSize().height);
 	}
 
-	private void moveFood()
-	{
-		food.setLocation(foodLocationX, foodLocationY);
-		//window.add(food);
-	}
+	// TurtlePanel window = Tortoise.getBackgroundWindow();
 
-	//TurtlePanel window = Tortoise.getBackgroundWindow();
-
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		FeedTortoise2Player feeder = new FeedTortoise2Player();
 		feeder.controlTheTortoise();
-		feeder.feedTheTortoise();
+		if(x < 2){
+			x += 1;
+			robot.setX(500);
+			robot.setY(500);
+			robot2.setX(0);
+			robot2.setY(250);
+		}
 	}
 
-	private void controlTheTortoise()
-	{
-		KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(this);
-		//Tortoise.show();
-		//Tortoise.setSpeed(10);
-//Tortoise.getBackgroundWindow().setBackground(Colors.getRandomColor());
-
+	private void controlTheTortoise() {
+		KeyboardFocusManager.getCurrentKeyboardFocusManager()
+				.addKeyEventDispatcher(this);
+		robot2.changeRobot("/Users/Guest/Desktop/Level1Project/Robot2.3");
 	}
 
-	private void feedTheTortoise()
-	{
-		moveFood();
-	}
-	
-	private boolean processEvent()
-	{
-		try
-		{
+	private boolean processEvent() {
+		try {
 			checkIfFoodFound();
-		} catch (Exception e)
-		{
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return false;
 	}
 
 }
-
-
