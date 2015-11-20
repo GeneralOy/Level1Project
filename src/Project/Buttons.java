0ac172a8632a3bda725feb931170ccd0539ac1e4
PhazeIllusions <league@iMac.attlocal.net>
2015-11-19 17:00:38 -0800

    qw
    qw

--- src/Project/Buttons.java
+++ src/Project/Buttons.java
@@ -1,5 +1,16 @@
 package Project;
 
-public class Buttons {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
 
+public class Buttons {
+	private static JFrame frame = new JFrame();
+	private static JPanel mainPanel = new JPanel();
+	private static JButton nastyButton = new JButton();
+	public static void main(String[] args) {
+		frame.add(mainPanel);
+		mainPanel.setLayout(GridLayout(1,2));
+		mainPanel.add(comp)
+	}
 }
