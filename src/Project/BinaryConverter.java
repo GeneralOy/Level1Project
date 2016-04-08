e09970596c25aabd433e80a0e97139b662c8d6bd
No One <No.Ones.Email@gmail.com>
2016-04-07 17:00:24 -0700

    f
    d

--- src/Project/BinaryConverter.java
+++ src/Project/BinaryConverter.java
@@ -10,72 +10,61 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class BinaryConverter implements ActionListener {
+	// Text Fields
+	JTextField answer = new JTextField(20);
+	JTextField question = new JTextField(20);
+	String questionString;
+	// Frame
+	JFrame mainFrame = new JFrame();
+	// Button
+	JButton convert = new JButton();
+	// Panels 3 2 1
+	// JPanel panel1 = new JPanel();
+	// JPanel panel2 = new JPanel();
+	JPanel panel3 = new JPanel();
+
 	public static void main(String[] args) {
-		// Text Fields
-		JTextField answer = new JTextField(20);
-		JTextField question = new JTextField(20);
+		BinaryConverter main = new BinaryConverter();
+		main.Start();
+	}
+
+	public void Start() {
 		// Frame
-		JFrame mainFrame = new JFrame();
 		mainFrame.show();
 		// Button
-		JButton convert = new JButton();
 		convert.addActionListener(this);
 		convert.setText("Convert");
 		// Panels 3 2 1
-		// JPanel panel1 = new JPanel();
-		// JPanel panel2 = new JPanel();
-		JPanel panel3 = new JPanel();
 		mainFrame.add(panel3);
 		panel3.add(question);
 		panel3.add(convert);
 		panel3.add(answer);
 		mainFrame.pack();
-		
-	}
-	public BinaryConverter() {
-		// Text Fields
-				JTextField answer = new JTextField(20);
-				JTextField question = new JTextField(20);
-				// Frame
-				JFrame mainFrame = new JFrame();
-				mainFrame.show();
-				// Button
-				JButton convert = new JButton();
-				convert.addActionListener(this);
-				convert.setText("Convert");
-				// Panels 3 2 1
-				// JPanel panel1 = new JPanel();
-				// JPanel panel2 = new JPanel();
-				JPanel panel3 = new JPanel();
-				mainFrame.add(panel3);
-				panel3.add(question);
-				panel3.add(convert);
-				panel3.add(answer);
-				mainFrame.pack();
 	}
 
-	int convert(String binary) {
+	char convert(String binary) {
 		if (binary.length() != 8) {
 			JOptionPane.showMessageDialog(null, "Enter 8 bits, silly!!!");
 			return 0;
 		}
-		if (!Pattern.matches("\2", binary)) {
-			JOptionPane.showMessageDialog(null, "Binary can only contain 1s or 0s, silly!!!");
-			return 0;
-		}
 		try {
 			int asciiValue = Integer.parseInt(binary, 2);
-			return asciiValue;
+			return (char) asciiValue;
 		} catch (Exception e) {
 			JOptionPane.showMessageDialog(null, "Enter a binary, silly!!!");
 			// question.setText("");
 			return 0;
 		}
 	}
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		
+		questionString = question.getText();
+		char Converted = convert(questionString);
+		String fullConvert = Character.toString(Converted);
+		answer.setText(fullConvert);
+
 	}
 
 }
